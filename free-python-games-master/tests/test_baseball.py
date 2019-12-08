a7c61d38942cf7bf3c460cce26e27e79db4d9694
tenlif <tenacity18@hotmail.com>
2019-12-08 13:45:08 +0900

    [UPDATE] Changed answer check words
    Changed words of checking inputs.
    Right position -> strike
    Right number -> ball

--- free-python-games-master/tests/test_baseball.py
+++ free-python-games-master/tests/test_baseball.py
@@ -1,13 +1,14 @@
 """Baseball, Guess N digit number.
+Strike : Right number, right position. Ball : Right number in input
 
 Exercises
 1. How can we make it difficult?
 2. How many digits are most difficult?
 3. Change it to allow repeated numbers.
-
 """
 
 import random
+
 
 def get_num_digit():
     digits = 0
@@ -37,7 +38,7 @@ def get_user_ans(right_ans_):
         if user_input[pos] < '0' or user_input[pos] > '9':
             valid = True
     while valid:
-        user_input = input("Please Enter Only Numbers. Enter %d-digits : " % len(right_ans_))
+        user_input = input("Enter Only Numbers. Enter %d-digits : " % len(right_ans_))
         valid = False
         for pos in range(len(user_input)):
             if user_input[pos] < '0' or user_input[pos] > '9':
@@ -65,12 +66,12 @@ def check_format(user_ans_, right_ans_):
 
 
 def compare_user_ans(right_ans_, user_ans_):
-    check_result = {'right number': 0, 'right position': 0}
+    check_result = {'strike': 0, 'ball': 0}
     for pos in range(len(right_ans_)):
         if user_ans_[pos] == right_ans_[pos]:
-            check_result['right position'] += 1
+            check_result['strike'] += 1
         elif user_ans_[pos] in right_ans_:
-            check_result['right number'] += 1
+            check_result['ball'] += 1
     return check_result
 
 
@@ -83,15 +84,13 @@ while not correct:
     user_ans = get_user_ans(right_ans)
     if check_format(user_ans, right_ans):
         checked_ans = compare_user_ans(right_ans, user_ans)
-        if checked_ans['right position'] == len(right_ans):
+        if checked_ans['strike'] == len(right_ans):
             print("You got right answer : ", end='')
             for position in range(len(user_ans)):
                 print(user_ans[position], end='')
             print("  with %d trials" % trials)
             correct = True
         else:
-            print("Right Number : %s\nRight Position : %s\n%d Trials"
-                  % (checked_ans['right number'], checked_ans['right position'], trials))
+            print("Strike : %s\tBall : %s\n%d Trials"
+                  % (checked_ans['strike'], checked_ans['ball'], trials))
             trials += 1
-
-print("The End")
